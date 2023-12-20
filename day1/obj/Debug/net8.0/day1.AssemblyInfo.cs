86f1589ba76a2164c57437bb5eb31c5d5c79b5da
pranavkharche79 <pranavkharche79@gmail.com>
2023-12-20 21:34:35 +0530

    hi

--- day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
+++ day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee89c51ded2fcf35fb01c627e117d17bb3dd99e")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
